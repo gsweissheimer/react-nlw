e65e5deb3b5ded99c885fca514dca108c13daeb9
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-05-01 11:24:09 -0300

    refactor: remove Terminal component from routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,6 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import PrivatedRoutes from './PrivatedRoutes';
 import Login from '../pages/Login/Login';
 import Logout from '../pages/Login/logout';
-import Terminal from '../pages/Terminal/Terminal';
 
 const AppRoutes = () => {
     const navigate = useNavigate();
@@ -16,7 +15,6 @@ const AppRoutes = () => {
     return (
         <>
             <Routes>
-                <Route path="/meu-amigo" element={<Terminal />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/logout" element={<Logout />} />
                 <Route path="/*" element={<PrivatedRoutes />} />
