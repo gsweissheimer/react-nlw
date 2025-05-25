15ab45f2b79c81319f4f42503630720e9dac549f
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-05-25 08:44:36 -0300

    fix: update API base URL to production environment

--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -3,7 +3,7 @@ import Cookies from 'js-cookie';
 
 // Criação de uma instância do Axios
 const api = axios.create({
-    baseURL: 'http://localhost:3001/miau', // Coloque sua URL base aqui
+    baseURL: 'https://nodejs-nlw-production.up.railway.app/miau', // Coloque sua URL base aqui
 });
 
 // Adicionando o token Bearer automaticamente
